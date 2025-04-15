d9b40989bf8345cee3ad3eb2c302e5e29b948a08
prince kumar <princekr969@gmail.com>
2025-04-15 13:47:54 +0530

    Config vite.cofig.js

--- Frontend/EdTech-Frontend/vite.config.js
+++ Frontend/EdTech-Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8012",
+      "/api": "https://winter-project-edtech.onrender.com",
     }
   },
   plugins: [react()],
