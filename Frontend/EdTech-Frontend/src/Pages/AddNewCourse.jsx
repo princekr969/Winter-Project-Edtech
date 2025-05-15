16de6bdf382ad43666d851f32d4db29c0353c3af
prince kumar <princekr969@gmail.com>
2025-05-15 15:01:17 +0530

    add the navigation feature in add course page

--- Frontend/EdTech-Frontend/src/Pages/AddNewCourse.jsx
+++ Frontend/EdTech-Frontend/src/Pages/AddNewCourse.jsx
@@ -6,6 +6,8 @@ import {
   Video,
 } from "lucide-react";
 import courseService from '../services/course';
+import { useNavigate } from 'react-router-dom';
+import { ErrorPopup } from '../components';
 
 
 function AddNewCourse() {
@@ -31,9 +33,11 @@ function AddNewCourse() {
     ];
 
     const imageInputRef = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [popUp, setPopUp] = useState(false);
+    const navigate = useNavigate();
 
     const [previewImage, setPreviewImage] = useState(formData.imageUrl || "");
-    
     
     const handleImageUpload = (event) => {
     const file = event.target.files[0];
@@ -49,6 +53,7 @@ function AddNewCourse() {
         console.log("formData: ", formData);
 
         try {
+            setLoading(true);
             const data = new FormData();
             data.append("title", formData.title);
             data.append("description", formData.description);
@@ -56,7 +61,16 @@ function AddNewCourse() {
             data.append("category", formData.category);
             data.append("imageUrl", formData.imageUrl);
             const res = await courseService.createCourse(data);
-            console.log(res);
+            if(res.success){
+                setPopUp(true);
+                setTimeout(() => {
+                    navigate("/user/created")
+                    setPopUp(false);
+                }, 5000)
+            }
+            setLoading(false);
+
+            console.log("course Created",res);
         } catch (error) {
             console.log("Add new course error", error)   
         }
@@ -67,15 +81,16 @@ function AddNewCourse() {
     }
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <main className="max-w-4xl mt-20 mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <>
+    <div className=" bg-gray-50">
+      <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <form
           className="space-y-6"
           onSubmit={handleFormSubmit}
         >
           <div className="bg-white rounded-lg shadow">
             <div className="p-6 space-y-6">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 gap-6">
                 <div className="md:col-span-2">
                   <label className="block text-sm font-medium text-gray-700 mb-1">
                     Title
@@ -190,9 +205,34 @@ function AddNewCourse() {
             <button
               type="submit"
               className="px-6 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 flex items-center gap-2 font-medium"
-            >
-              <Save className="w-4 h-4" />
-              Create Course
+            >   
+                {(!loading)?<Save className="w-4 h-4" /> :null}
+                {(loading) ?
+                    <div className="flex items-center justify-center">
+                    <svg
+                      className="w-5 h-5 mr-2 text-white animate-spin"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 01-8 8z"
+                      ></path>
+                    </svg>
+                    Processing...
+                  </div>
+                  : 
+                  ("Create Course")}
             </button>
           </div>
 
@@ -200,6 +240,9 @@ function AddNewCourse() {
       </main>
       
     </div>
+    {(popUp)?<ErrorPopup type="success" duration={5000} message={"Course Created successfully"}/>:null}
+    </>
+    
   )
 }
 
