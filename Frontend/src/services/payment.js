e49e5866f9de785ff73bc6291343cf0530b2819e
prince kumar <princekr969@gmail.com>
2025-05-29 08:37:41 +0530

    qr fetch api update

--- Frontend/src/services/payment.js
+++ Frontend/src/services/payment.js
@@ -24,6 +24,6 @@ export class PaymentServices{
     }
 }
 
-const paymentService = new PaymentServices("https://winter-project-edtech.onrender.com/api/v1/course");
+const paymentService = new PaymentServices("https://winter-project-edtech.onrender.com/api/v1/payment");
 
 export default paymentService;
